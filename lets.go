fd4a7c21e08a38010a8d5003972bd4243dd2f39e
Shreyas <shreyas@zendeals.com>
2013-08-31 17:51:00 -0700

    interfaces

--- lets.go
+++ lets.go
@@ -148,5 +148,39 @@ func learnFlowControl() {
     goto love
 love:
 
-    //learnInterfaces() // Good stuff coming up!
+    learnInterfaces() // Good stuff coming up!
+}
+// Define Stringer as an interface type with one method, String.
+type Stringer interface {
+    String() string
+}
+
+// Define pair as a struct with two fields, ints named x and y.
+type pair struct {
+    x, y int
+}
+
+// Define a method on type pair.  Pair now implements Stringer.
+func (p pair) String() string { // p is called the "receiver"
+    // Sprintf is another public function in package fmt.
+    // Dot syntax references fields of p.
+    return fmt.Sprintf("(%d, %d)", p.x, p.y)
+}
+
+func learnInterfaces() {
+    // Brace syntax is a "struct literal."  It evaluates to an initialized
+    // struct.  The := syntax declares and initializes p to this struct.
+    p := pair{3, 4}
+    fmt.Println(p.String()) // call String method of p, of type pair.
+    var i Stringer          // declare i of interface type Stringer.
+    i = p                   // valid because pair implements Stringer
+    // Call String method of i, of type Stringer.  Output same as above.
+    fmt.Println(i.String())
+
+    // Functions in the fmt package call the String method to ask an object
+    // for a printable representation of itself.
+    fmt.Println(p) // output same as above. Println calls String method.
+    fmt.Println(i) // output same as above
+
+    //learnErrorHandling()
 }
